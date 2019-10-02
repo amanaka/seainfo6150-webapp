4bcb1371315fe991ad4728685dfec7b5e2c686cd
Amanda Tang <amandatang@cybercruiser.local>
2019-10-01 20:52:26 -0700

    assignment 4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
 import ArticleList from "./ArticleList/ArticleList";
 import articles from "./data/articles.json";
-=======
-import PropTypes from 'prop-types';
->>>>>>> 9dcf61c405eddd096cc274c88174c6529e6b7ff1
 
-const App = ({ article }) => {
+function App() {
   return (
     <div className="App">
       <ArticleList articles={Object.values(articles)} />
@@ -14,13 +10,4 @@ const App = ({ article }) => {
   );
 }
 
-App.propTypes = {
-  article: PropTypes.shape({
-    "title": PropTypes.string.isRequired,
-    "shortText": PropTypes.string.isRequired,
-    "pubDate": PropTypes.string.isRequired,
-    "pubYear": PropTypes.string.isRequired,
-    "author": PropTypes.string.isRequired
-  }).isRequired
-}
 export default App;

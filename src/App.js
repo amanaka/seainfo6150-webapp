962784db9a34a185a41275a5331f76e7524a70b1
luyang <46611524+amanaka@users.noreply.github.com>
2019-10-07 17:49:18 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import ArticleList from "./ArticleList/ArticleList";
+import articles from "./data/articles.json";
 
-const App = ({ articles }) => {
-  return <div className="App">
-    
-  </div>;
-};
+function App() {
+  return (
+    <div className="App">
+      <ArticleList articles={Object.values(articles)} />
+    </div>
+  );
+}
 
-App.propTypes = {
-  articles: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      shortText: PropTypes.string.isRequired,
-      pubDate: PropTypes.string.isRequired,
-      pubYear: PropTypes.string.isRequired,
-      author: PropTypes.string.isRequired
-    })
-  ).isRequired
-};
 export default App;

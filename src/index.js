aa99ad1b1791f865c47b10803639d77fb63d23fc
luyang <46611524+amanaka@users.noreply.github.com>
2019-10-07 17:50:24 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import Demo from "./Demo";
-import articles from "./data/articles.json";
 
-const querystring = window.location.search || "";
+const querystring = window.location.search || '';
 
-if (querystring.toLowerCase() === "?demo=true") {
-  ReactDOM.render(<Demo />, document.getElementById("root"));
+if (/demo/.test(querystring.toLowerCase())) {
+    ReactDOM.render(<Demo querystring={querystring} />, document.getElementById('root'));
 } else {
-  ReactDOM.render(
-    <App articles={Object.values(articles)} />,
-    document.getElementById("root")
-  );
+    ReactDOM.render(<App />, document.getElementById('root'));
 }
